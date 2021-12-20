3fe04f2de9cf4857c0fd04feee1926a62cc829a3
Rana Yalcin <yalcinrana06@gmail.com>
2021-12-20 14:01:03 +0300

    Ana Depo Sayfasını oluşturdum
    Ana depoda ürünlerin kodunu,id sini,ismini ve stok durumunu gösteren tabloyu oluşturdum

--- app.js
+++ app.js
@@ -1,20 +1,103 @@
-window.addEventListener('load', () =>{
-  window.vue = new Vue({
-        el: '#app',
-        data:{
-            isLoading: true,
-            product:[]
+const anasayfa={template: `<h1>Burası Ana Sayfa</h1>`}
+const anadepo={template: `
+<table class="table table-striped">
+<thead class="thead-dark">
+ <tr>
 
-        },
-        created(){
-            fetch('./data.json')
-            .then((res) => {return res.json()})
-            .then((res) => {this.isLoading=false;
+     <th>
+         Ürün Id
+     </th>
+     <th>
+         Ürün Kodu
+     </th>
+     <th>
+         Ürün Adı
+     </th>
+     <th>
+         Stock Durumu
+     </th>
+     <th>
+       Ürün ekle/sil
+     </th>
+
+  </tr>
+  </thead>
+  <tbody>
+  <tr v-for="prdc in product">
+   <td>{{prdc.id}}</td>
+   <td>{{prdc.code}}</td>
+   <td>{{prdc.name}}</td>
+   <td>{{prdc.stock}}</td>
+   <td> <button type="button" class="btn btn-outline-primary mr-1">Ürün ekle
+   </button>
+   <button type="button" class="btn btn-outline-danger mr-1">Ürün Sil
+   </button>
+   </td>
+
+   
+     
+  </tr>
+  </tbody>
+
+</table>`,
+data(){
+    return{
+        product:[]
+    }
+},
+methods:{
+    created(){
+        fetch('./data.json')
+        .then((res) => {return res.json()})
+        .then((res) => {this.isLoading=false;
+            
+            this.product=res.product;
+        
+        })
+        
+    }
+
+},
+mounted:function(){
+    this.created();
+}}
+const subedepo={template: `<h1>Burası Şube Depo ile ilgili</h1>`}
+const routes=[
+    {path:'/anasayfa',component: anasayfa},
+    {path:'/anadepo',component:anadepo},
+    {path:'/subedepo',component:subedepo}
+
+]
+const router=new VueRouter({
+    routes
+})
+const app= new Vue({
+    router
+}).$mount('#app')
+// window.addEventListener('load', () =>{
+    
+//     window.vue = new Vue({
+        
+//         data:{
+//             isLoading: true,
+
+//             product:[]
+
+//         },
+//         methods: {
+//           deleteFromProduct(index){
+//                this.product.splice(index,1);
+//            }
+//         },
+//         created(){
+//             fetch('./data.json')
+//             .then((res) => {return res.json()})
+//             .then((res) => {this.isLoading=false;
                 
-                this.product=res.product;
+//                 this.product=res.product;
             
-            })
+//             })
             
-        }
-    })
-});
+//         }
+//     })
+// }); 
