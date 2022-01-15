a6de39eec0ba2180542b74134c6a55051d5f111b
Rana Yalçın <77885953+DNA-RNA@users.noreply.github.com>
2022-01-15 13:59:18 +0300

    Tabloları temsil eden Entities katmanı

---
+++ Entities/Concrete/Role.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Role : IEntity
+   {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+       
+    }
+}
