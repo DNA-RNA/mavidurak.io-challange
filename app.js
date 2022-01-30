e044fdcb4902f08dc8ff2d044ce795d653985688
Rana Yalcin <yalcinrana06@gmail.com>
2022-01-30 20:38:25 +0300

    Ön yüz bağlantısı tamamlandı

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ data-bs-target="#exampleModal" v-on:click="addClick()">Ürün Ekle</button>
    <td> <button type="button" class="btn btn-outline-primary mr-1" data-bs-toggle="modal"
    data-bs-target="#exampleModal" v-on:click="editClick(prdc)">Ürün Güncelle
    </button>
-   <button type="button"  v-on:click="editClick(prdc,index)" class="btn btn-outline-danger mr-1">Ürün Sil
+   <button type="button"  v-on:click="deleteClick(prdc.id)" class="btn btn-outline-danger mr-1">Ürün Sil
    </button>
    </td>   
   </tr>
@@ -121,7 +121,7 @@ aria-labelledby="exampleModalLabel" aria-hidden="true">
  </div>
 
      <button type="button" v-on:click="createClick()" v-if="id==0" class="btn btn-primary">Ekle</button>
-     <button type="button" v-on:click="updateClick()" v-if="id!=0" class="btn btn-primary">Güncelle</button>
+     <button type="button" v-on:click="updateClick(prdc.id)" v-if="id!=0" class="btn btn-primary">Güncelle</button>
 </div>
 </div>
 
@@ -135,7 +135,182 @@ data(){
         product:[],
         modalTitle:"",
         id:0,
-        name:""
+        roleId,
+        categoryId,
+        salePrice,
+        name:"",
+        code:"",
+        stock,
+        purchaseDate,
+        purchasePrice
+    }
+},
+methods:{
+    //ürünleri getir - getall
+    refreshData(){
+        axios.get(variables.API_URL + "products")
+        .then((response)=>{this.product=response.data});
+        
+    }
+    ,
+    //createClick için gerekli dataları alan fonksiyon
+    addClick(){
+        this.modalTitle="Ürün Ekle" ;
+        this.id=0;
+        this.name="";
+        this.roleId=0;
+        this.categoryId=0;
+        this.salePrice=300;
+        this.purchasePrice=400;
+        this.code="";
+        this.purchaseDate=Date;
+        this.stock=500;
+
+        
+    }, 
+    //updateClick  için düzenlenecek dataları alan fonksiyon
+    editClick(prdc){
+        this.modalTitle="Ürün Güncelle" ;
+        this.id=prdc.id;
+        this.name=prdc.product_name;
+        this.roleId=prdc.role_id;
+        this.categoryId=prdc.category_id;
+        this.salePrice=prdc.sale_price;
+        this.purchasePrice=prdc.purchase_price;
+        this.code=prdc.product_code;
+        this.stock=prdc.stock;
+        this.purchaseDate=prdc.purchase_date;
+
+    },
+    createClick(){
+        axios.post(variables.API_URL + "products",{
+            product_name:this.name,
+            id:this.id,
+            role_id:this.roleId,
+            category_id:this.categoryId,
+            sale_price:this.salePrice,
+            purchase_price:this.purchasePrice,
+            product_code:this.code,
+            stock:this.stock,
+            purchase_date:this.purchaseDate
+        }).then((response)=>{this.refreshData();});
+       
+    },
+    updateClick(id){
+        axios.put(variables.API_URL + "products/"+ id,{
+        
+            product_name:this.name,
+            id:this.id,
+            role_id:this.roleId,
+            category_id:this.categoryId,
+            sale_price:this.salePrice,
+            purchase_price:this.purchasePrice,
+            product_code:this.code,
+            stock:this.stock,
+            purchase_date:this.purchaseDate
+        })
+        
+        .then((response)=>{this.refreshData();});
+    },
+    deleteClick(id){
+        axios.delete(variables.API_URL + "products/"+id)
+        .then((response)=>{this.product=response.data});
+    }
+
+},
+mounted:function(){
+    this.refreshData();
+}}
+const subedepo={template: `<div>
+
+<button type="button" class="btn btn-primary m-2 fload-end" data-bs-toggle="modal"
+data-bs-target="#exampleModal" v-on:click="addClick()">Ürün Ekle</button>
+<table class="table table-striped">
+<thead class="thead-dark">
+ <tr>
+
+     <th>
+         Ürün Id
+     </th>
+     <th>
+         Ürün Adı
+     </th>
+     <th>
+         Ürün Kodu
+     </th>
+     <th>
+         Stock Durumu
+     </th>
+     <th>
+         Alış Fiyatı
+     </th>
+     <th>
+          Satış Fiyatı
+      </th>
+
+     <th>
+       Ürün güncelle/sil
+     </th>
+
+  </tr>
+  </thead>
+  <tbody>
+  <tr v-for="prdc in product">
+   <td>{{prdc.id}}</td>
+   <td>{{prdc.product_name}}</td>
+   <td>{{prdc.prdct_code}}</td>
+   <td>{{prdc.stock}}</td>
+   <td>{{prdc.purchase_price}}</td>
+   <td>{{prdc.sale_price}}</td>
+   <td> <button type="button" class="btn btn-outline-primary mr-1" data-bs-toggle="modal"
+   data-bs-target="#exampleModal" v-on:click="editClick(prdc)">Ürün Güncelle
+   </button>
+   <button type="button"  v-on:click="deleteClick(prdc.id)" class="btn btn-outline-danger mr-1">Ürün Sil
+   </button>
+   </td>   
+  </tr>
+  </tbody>
+  </thead>
+</table> 
+
+<div class="modal fade" id="exampleModal" tabindex="-1"
+aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-lg modal-dialog-centered">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">{{modalTitle}}</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Kapat"></button>
+
+</div>
+<div class="modal-body">
+ <div class="input-group mb-3">
+    <span class="input-group-text">Ürün İsmi</span>
+    <input type="text" class="form-control" v-model="name">
+ </div>
+
+     <button type="button" v-on:click="createClick()" v-if="id==0" class="btn btn-primary">Ekle</button>
+     <button type="button" v-on:click="updateClick(prdc.id)" v-if="id!=0" class="btn btn-primary">Güncelle</button>
+</div>
+</div>
+
+</div>
+</div>
+
+
+</div>`,
+data(){
+    return{
+        product:[],
+        modalTitle:"",
+        id:0,
+        roleId,
+        categoryId,
+        salePrice,
+        name:"",
+        code:"",
+        stock,
+        purchaseDate,
+        purchasePrice
     }
 },
 methods:{
@@ -150,33 +325,68 @@ methods:{
         this.modalTitle="Ürün Ekle" ;
         this.id=0;
         this.name="";
+        this.roleId=0;
+        this.categoryId=0;
+        this.salePrice=300;
+        this.purchasePrice=400;
+        this.code="";
+        this.purchaseDate=Date;
+        this.stock=500;
+
+        
     },
-    editClick(prdct){
+    editClick(prdc){
         this.modalTitle="Ürün Güncelle" ;
-        this.id=prdct.id;
-        this.name=prdct.name;
+        this.id=prdc.id;
+        this.name=prdc.product_name;
+        this.roleId=prdc.role_id;
+        this.categoryId=prdc.category_id;
+        this.salePrice=prdc.sale_price;
+        this.purchasePrice=prdc.purchase_price;
+        this.code=prdc.product_code;
+        this.stock=prdc.stock;
+        this.purchaseDate=prdc.purchase_date;
 
     },
     createClick(){
         axios.post(variables.API_URL + "products",{
-            name:this.name
-        })
-        .then((response)=>{this.refreshData();});
-    },
-    updateClick(){
-        axios.put(variables.API_URL + "products/1",{
+            product_name:this.name,
             id:this.id,
-            name:this.name
+            role_id:this.roleId,
+            category_id:this.categoryId,
+            sale_price:this.salePrice,
+            purchase_price:this.purchasePrice,
+            product_code:this.code,
+            stock:this.stock,
+            purchase_date:this.purchaseDate
+        }).then((response)=>{this.refreshData();});
+       
+    },
+    updateClick(id){
+        axios.put(variables.API_URL + "products/"+ id,{
+        
+            product_name:this.name,
+            id:this.id,
+            role_id:this.roleId,
+            category_id:this.categoryId,
+            sale_price:this.salePrice,
+            purchase_price:this.purchasePrice,
+            product_code:this.code,
+            stock:this.stock,
+            purchase_date:this.purchaseDate
         })
         
         .then((response)=>{this.refreshData();});
+    },
+    deleteClick(id){
+        axios.delete(variables.API_URL + "products/"+id)
+        .then((response)=>{this.product=response.data});
     }
 
 },
 mounted:function(){
     this.refreshData();
 }}
-const subedepo={template: `<h1>Burası Şube Depo ile ilgili</h1>`}
 const routes=[
     {path:'/anasayfa',component: anasayfa},
     {path:'/anadepo',component:anadepo},
